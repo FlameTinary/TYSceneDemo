9995b98db1d5642ed4ae880653e895027828e2b5
tianyu <tianyu@meishubao.com>
2019-06-01 16:56:15 +0800

    添加相关代码

--- TYSceneDemo/TYSceneDemo/ViewController.m
+++ TYSceneDemo/TYSceneDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<ARSCNViewDelegate>
+
+@property (weak, nonatomic) IBOutlet SCNView *sceneView;
 
 @end
 
@@ -17,6 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.sceneView.delegate = self;
+    
+//    self.sceneView.showsStatistics = YES;
+    
+    SCNScene *scene = [SCNScene sceneNamed:@"ship.scnassets/myship/myship.scn"];
+    
+    // 添加相机
+    SCNNode *cameraNode = [SCNNode node];
+    cameraNode.camera = [SCNCamera camera];
+    [scene.rootNode addChildNode:cameraNode];
+    
+    // 调整视角
+    cameraNode.position = SCNVector3Make(0, 0, 15);
+    
+    // 添加灯光
+    SCNNode * lightNode = [SCNNode node];
+    lightNode.light = [SCNLight light];
+    lightNode.light.type = SCNLightTypeOmni;
+    lightNode.position = SCNVector3Make(0, -10, 10);
+    [scene.rootNode addChildNode:lightNode];
+    
+    // 添加氛围灯光
+    SCNNode *ambientLightNode = [SCNNode node];
+    ambientLightNode.light = [SCNLight light];
+    ambientLightNode.light.type = SCNLightTypeAmbient;
+    ambientLightNode.light.color = [UIColor blackColor];
+    [scene.rootNode addChildNode:ambientLightNode];
+    
+    
+    SCNNode *myship = [scene.rootNode childNodeWithName:@"myship" recursively:YES];
+    
+    [myship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
+    
+    self.sceneView.scene = scene;
+    
+    self.sceneView.allowsCameraControl = YES;
+    
+    
+    
 }
 
 
